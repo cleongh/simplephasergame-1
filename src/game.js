f63dd88fd2fb15ede0551a55dc747419a996cf39
Carlos León <cleon@ucm.es>
2022-02-02 12:05:34 +0100

    añadidas definiciones de tipos VSCode

--- src/game.js
+++ src/game.js
@@ -8,7 +8,7 @@ import Level from './level.js';
  */
 let config = {
     type: Phaser.AUTO,
-    width:  1000,
+    width: 1000,
     height: 500,
     scale: {
         // mode: Phaser.Scale.FIT,  
@@ -16,12 +16,12 @@ let config = {
     },
     pixelArt: true,
     scene: [Boot, Level, End],
-    physics: { 
-        default: 'arcade', 
-        arcade: { 
-            gravity: { y: 400 }, 
-            debug: false 
-        } 
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 400 },
+            debug: false
+        }
     }
 };
 
